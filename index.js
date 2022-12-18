ec0ee723fab5808d06e9f47b4d04cf2ac9c0cec2
luquilo <luquilo.etc@gmail.com>
2022-12-18 22:01:42 +0700

    mengakses data dari database menggunakan get http request

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import db from './connection.js'
+import response from './response.js';
 
 const app = express();
 const port = 3000;
-import bodyParser from 'body-parser';
 
 app.use(bodyParser.json())
 
 app.get('/', (req,res) => {
-    res.send('utamaa')
+    const sql = 'select * from mahasiswa';
+
+    db.query(sql, (error, result) => {
+        //hasil data dari mysql
+        response(200, result, 'get all data from mahasiswa', res)
+    })
 })
 
-app.get('/hello', (req,res) => {
-    console.log({ urlparams : req.query })
-    res.send('hello page')
+app.get('/find', (req,res) => {
+    const sql = `select nama_lengkap from mahasiswa where nim =  ${req.query.nim}`
+    db.query(sql, (error, result) => {
+        response(200, result, "find mahasiswa name", res)
+    })
 })
 
 app.post('/login', (req,res) => {
